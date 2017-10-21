ebcaed6d24884ec9731f7f3553350c258e2e009a
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-10-21 03:11:26 -0200

    Add files via upload

--- script.js
+++ script.js
@@ -5,21 +5,22 @@ onload = startingWebsite
 function startingWebsite() {
   //console.log("Starting website");
 
-  allSecrets = document.getElementsByClassName("secret");
-  for(var i = 0; i < allSecrets.length; i++) {
-    allSecrets[i].addEventListener("mouseover", showSecret);
-    allSecrets[i].addEventListener("mouseout", hideSecret);
-  }
+  defineAge();
 }
 
-function showSecret(event) {
-  //console.log("Showing secret");
-  secret = event.target;
-  secret.style["background-color"] = "white";
-}
+function defineAge() {
+  today = new Date();
+  birthdayDate = new Date();
 
-function hideSecret(event) {
-  //console.log("Hiding secret");
-  secret = event.target;
-  secret.style["background-color"] = "black";
+  birthdayDate.setFullYear(1992, 10, 9); // 12
+  ageNumber = today.getFullYear() - birthdayDate.getFullYear();
+
+  if(today.getMonth() <= birthdayDate.getMonth() || today.getDate() <= birthdayDate.getDate)
+    ageNumber--;
+
+  birthday = document.getElementById("birthday");
+  birthday.innerHTML = birthdayDate.getDate() + "/" + (birthdayDate.getMonth()+1) + "/" + birthdayDate.getFullYear();
+
+  age = document.getElementById("age");
+  age.innerHTML = ageNumber + " anos";
 }
