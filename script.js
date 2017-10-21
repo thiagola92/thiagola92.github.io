3effeccb6483c0fe9b3c9f92bf19fa26d73e6cae
Thiago Lages de Alencar <thiagola92@users.noreply.github.com>
2017-10-20 22:12:24 -0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,25 @@
-console.log("Loading script.js");
+//console.log("Loading script.js");
 
 onload = startingWebsite
 
 function startingWebsite() {
-  console.log("Starting website");
+  //console.log("Starting website");
 
-  document.getElementById("secret").addEventListener("mouseover", showSecret);
-  document.getElementById("secret").addEventListener("mouseout", hideSecret);
+  allSecrets = document.getElementsByClassName("secret");
+  for(var i = 0; i < allSecrets.length; i++) {
+    allSecrets[i].addEventListener("mouseover", showSecret);
+    allSecrets[i].addEventListener("mouseout", hideSecret);
+  }
 }
 
-function showSecret() {
-  console.log("Showing secret");
-  
+function showSecret(event) {
+  //console.log("Showing secret");
+  secret = event.target;
+  secret.style["background-color"] = "white";
 }
 
-function hideSecret() {
-  console.log("Hiding secret");
-
+function hideSecret(event) {
+  //console.log("Hiding secret");
+  secret = event.target;
+  secret.style["background-color"] = "black";
 }
